05da0062fab06304dffe96c1c859fe9cba06ac4f
pat-alt <altmeyerpat@gmail.com>
2023-10-08 09:09:49 +0200

    at least it's now tracking

---
+++ .julia/config/startup.jl
@@ -0,0 +1,13 @@
+# Setup OhMyREPL and Revise
+import Pkg
+let
+    pkgs = ["Revise", "OhMyREPL"]
+    for pkg in pkgs
+        if Base.find_package(pkg) === nothing
+            Pkg.add(pkg)
+        end
+    end
+end
+
+using Revise
+using OhMyREPL
