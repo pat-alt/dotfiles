fe5dbba6f9d14f5edb3440191b3688653ba94b93
pat-alt <altmeyerpat@gmail.com>
2023-10-11 10:25:27 +0200

    trying separate aliases file

---
+++ bash_custom/aliases/aliases.sh
@@ -0,0 +1,6 @@
+alias bashconfig="$EDITOR ~/.bashrc"
+alias ohmybash="$EDITOR ~/.oh-my-bash"
+alias home="cd ~/"
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/.git/ --work-tree=$HOME'
+alias restart='exec "$SHELL"'
+echo "Add aliases"
--- bash_custom/aliases/example.aliases.sh
+++ bash_custom/aliases/example.aliases.sh
@@ -1,7 +1,2 @@
 # Add your own custom alias in the custom/aliases directory. Aliases placed
 # here will override ones with the same name in the main alias directory.
-alias bashconfig="$EDITOR ~/.bashrc"
-alias ohmybash="$EDITOR ~/.oh-my-bash"
-alias home="cd ~/"
-alias config='/usr/bin/git --git-dir=$HOME/dotfiles/.git/ --work-tree=$HOME'
-alias restart='exec "$SHELL"'
